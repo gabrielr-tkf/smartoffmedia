80c1dcd8ff3097b2d513362f174dac1eae2eecfe
Gabriel Ramos <gabrielr@takeoffmedia.com>
2015-11-18 17:03:32 -0300

    LogController.cs, WebTestingController.cs

---
+++ Photon.WebAPI/Controllers/LogController.cs
@@ -0,0 +1,42 @@
+﻿using Photon.WebAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace Photon.WebAPI.Controllers
+{
+    public class LogController : ApiController
+    {
+        [System.Web.Http.AcceptVerbs("GET")]
+        public LogStateChangeResponse LogStateChange(int bathId, bool isOccupied)
+        {
+            LogStateChangeResponse response = new LogStateChangeResponse();
+           
+            try
+            {
+                // Store data in DB
+                if (HttpContext.Current.Cache["LogsTable"] == null)
+                {
+                    HttpContext.Current.Cache["LogsTable"] = new List<Tuple<int, bool, DateTime>>();
+                    ((List<Tuple<int, bool, DateTime>>)HttpContext.Current.Cache["LogsTable"]).Add(Tuple.Create(bathId, isOccupied, DateTime.Now));
+                }
+                else
+                {
+                    ((List<Tuple<int, bool, DateTime>>)HttpContext.Current.Cache["LogsTable"]).Add(Tuple.Create(bathId, isOccupied, DateTime.Now));
+                }
+                response.Status = "200";
+                response.Message = "State successfully logged";
+            }
+            catch (Exception ex)
+            {
+                response.Status = "500";
+                response.Message = "Internal Server Error";
+            }
+
+           return response;
+        }
+    }
+}
---
+++ Photon.WebAPI/Controllers/WebTestingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Photon.WebAPI.Controllers
+{
+    public class WebTestingController : Controller
+    {
+        [System.Web.Http.AcceptVerbs("GET")]
+        public ActionResult ShowStateChange()
+        {
+            //LogController lC = new LogController();
+            //lC.LogStateChange(bathId, isOccupied);
+
+            return View();
+        }
+    }
+}
