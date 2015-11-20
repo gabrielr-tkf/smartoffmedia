6e078cb6e1fe80d54db67276c91522f60e941f72
Andres Gasteasoro <andresg@takeoffmedia.com>
2015-11-20 19:00:47 -0300

    Chrome Extension

---
+++ Chrome Extension/popup.js
@@ -0,0 +1,64 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+
+var guid;
+
+  function SubscribeNotification() {
+            //var userId = guid;
+            var userId = "asdasd";
+			console.log(userId + " en subscribe to notification");
+            $.get("http://localhost:52325/Api/Notification/subscribe?bathid=1&userId=" + userId, function (data) {
+
+					console.log(data);
+
+           });
+        }
+		
+		     
+        $(function () {
+
+            $.connection.hub.url = "http://localhost:52325/signalr";
+
+            // Declare a proxy to reference the hub.
+            var chat = $.connection.photonHub;
+            // Create a function that the hub can call to broadcast messages.
+            chat.client.broadcastMessage = function (name, message) {
+                // Html encode display name and message.
+                var encodedName = $('<div />').text(name).html();
+                var encodedMsg = $('<div />').text(message).html();
+                // Add the message to the page.
+                $('#discussion').append('<li><strong>' + encodedName
+                    + '</strong>:&nbsp;&nbsp;' + encodedMsg + '</li>');
+            };
+            // Get the user name and store it to prepend to messages.
+            $('#displayname').val("Name");
+            // Set initial focus to message input box.
+            $('#message').focus();
+            // Start the connection.
+            $.connection.hub.start().done(function () {
+
+                chat.server.registerConId().done(function (result) {
+
+                    console.log(result);
+
+                    guid = result;
+
+                });;
+
+            });
+        });
+		
+		$("document").ready(function(){
+	
+	console.log("aaaa");
+	$("#ucSubscribe").bind("click", function(){
+		
+		
+		console.log("bbbb");
+		SubscribeNotification();
+		
+		});
+});
--- Photon.WebAPI/App_Start/Startup.cs
+++ Photon.WebAPI/App_Start/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNet.SignalR;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
 using Owin;
 using Photon.WebAPI;
 using System.Web.Routing;
@@ -10,9 +11,36 @@ namespace Photon.WebAPI
     {
         public void Configuration(IAppBuilder app)
         {
+            //RouteTable.Routes.MapHubs(new HubConfiguration()
+            //{
+            //    EnableJSONP = true,
+            //});
 
+            //app.MapSignalR();
 
-            app.MapSignalR();
+            // Branch the pipeline here for requests that start with "/signalr"
+            app.Map("/signalr", map =>
+            {
+                // Setup the CORS middleware to run before SignalR.
+                // By default this will allow all origins. You can 
+                // configure the set of origins and/or http verbs by
+                // providing a cors options with a different policy.
+                map.UseCors(CorsOptions.AllowAll);
+                var hubConfiguration = new HubConfiguration
+                {
+                     EnableDetailedErrors = true,
+                     EnableJavaScriptProxies = true,
+                     EnableJSONP = true
+                    // You can enable JSONP by uncommenting line below.
+                    // JSONP requests are insecure but some older browsers (and some
+                    // versions of IE) require JSONP to work cross domain
+                    // EnableJSONP = true
+                };
+                // Run the SignalR pipeline. We're not using MapSignalR
+                // since this branch already runs under the "/signalr"
+                // path.
+                map.RunSignalR(hubConfiguration);
+            });
         }
     }
 }
