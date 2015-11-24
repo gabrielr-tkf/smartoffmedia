aec4a43819da3db75b16474d6b002f7ff88c703d
Gabriel Ramos <gabrielr@takeoffmedia.com>
2015-11-24 11:54:34 -0300

    Adding "Constants.cs"

---
+++ Photon.WebAPI/Utilities/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Photon.WebAPI.Utilities
+{
+    public class Constants
+    {
+        public const string kkcoudFreeDB = "kkcloudFreeDB";
+
+        public const string OccupiedBaths = "OccupiedBaths";
+        public const string LastOccupiedTimes = "LastOccupiedTimes";
+        public const string BathQueues = "BathQueues";      
+    }
+}
