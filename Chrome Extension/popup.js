85deef1344083b1f0d51527f7d21bb90f4ede3de
Gabriel Ramos <gabrielr@takeoffmedia.com>
2016-01-08 10:50:14 -0300

    Setting the correct URL for the Chrome Extension

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,8 +1,8 @@
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//var API_BASE_URL = "http://kkcloud.azurewebsites.net";
- var API_BASE_URL = "http://localhost:52325/";
+var API_BASE_URL = "http://kkcloud.azurewebsites.net";
+// var API_BASE_URL = "http://localhost:52325/";
 
 
 
